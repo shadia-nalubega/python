5b1b768ee60919b2f7ec8c48ccf60f97c1b75986
shadia Nalubega <nalubegashadia50@gmail.com>
2026-03-21 14:20:26 +0300

    first commit

---
+++ shadia.py
@@ -0,0 +1,17 @@
+1
+#computer programming is the process of giving instructions to computers
+#memory partitioning is the process of creating smaller units of memory on the hard disc
+#memory address is an identifer of the memory cell on the hard disc. these memory cells are the partitioned blocks.
+#through uploading memory addresses onto ram and then the cpu gets them there,on RAM
+#a variable is an additional name to the memory address assigned by the programmer
+# a statement is a single complete instruction to the computer
+# a code simply means two or more statements to computer
+# a group of related codes is called a block of code
+2
+# no variable name starts with a number
+# no variable name is written in capital letters
+# we do not write variable names with unnecessary spaces
+# incase of compound variable names, we can use camel notations e.g myName and underscores e.g my_name
+3
+# declaring is the process of creating a variables
+# intialising is the process of giving a name to variable
