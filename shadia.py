b34a0bf0fbfcbed30fee64f2bd0f15e67c80539d
shadia Nalubega <nalubegashadia50@gmail.com>
2026-03-24 21:21:36 +0300

    modified

--- shadia.py
+++ shadia.py
@@ -1,17 +1,17 @@
 1
-#computer programming is the process of giving instructions to computers
-#memory partitioning is the process of creating smaller units of memory on the hard disc
-#memory address is an identifer of the memory cell on the hard disc. these memory cells are the partitioned blocks.
+#computer programming is the process of giving computers instructions on what to do
+#memory partitioning is the process of dividing current memory of the hard disc into smaller units of memory so as to create more memory space
+#memory address is a special identity given to a particular memory cell on the hard disc. Each memory cell has a different memory address
 #through uploading memory addresses onto ram and then the cpu gets them there,on RAM
-#a variable is an additional name to the memory address assigned by the programmer
-# a statement is a single complete instruction to the computer
-# a code simply means two or more statements to computer
-# a group of related codes is called a block of code
+#a variable is an additional name that points to values stored in the memory cells . it does not replace the memory address, but rather a simpler nme for the programmer to use when accessing values instead of the using the memory address.
+# a statement is that one complete instruction to the computer
+# a code simply means two or more statements to  a computer
+# a block of code simply means two or more related complete instructions to a computer. 
 2
 # no variable name starts with a number
 # no variable name is written in capital letters
 # we do not write variable names with unnecessary spaces
 # incase of compound variable names, we can use camel notations e.g myName and underscores e.g my_name
 3
-# declaring is the process of creating a variables
-# intialising is the process of giving a name to variable
+# declaring is the process of creating variables
+# intialising is the process of baptising a name to a variable. it is the same as assigning
